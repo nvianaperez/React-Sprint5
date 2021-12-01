71cf79de226576a3f343881861ab2e2d8057cb31
nviana <nvianaperez@gmail.com>
2021-12-01 13:39:19 +0100

    inici tasca

--- ApiTest/index.js
+++ ApiTest/index.js
@@ -1,5 +1,6 @@
-const apiUrl = "https://jsonplaceholder.typicode.com";
 /*
+const apiUrl = "https://jsonplaceholder.typicode.com";
+
 // llamar a una API con XMLHttpRequest
 
 //instancio objeto xhr, creo la función manejadora, y llamo a la función escuchando el evento load
@@ -56,20 +57,47 @@ xhr.send(); // envía y carga la respuesta y nos devuelve un status.
 // 0: {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz', address: {…}, …}
 */
 
+/*
+// llamar a una API con método FETCH, usando STRINGS para formatear el html
 
-// llamar a una API con método FETCH
+const apiUrl = "https://jsonplaceholder.typicode.com";
 
 //accedo al elemento HTML
 const HTMLelem = document.querySelector("#app");
 
 //llamo al método fetch que recibe la url y devuelve una promise. el resultado de la promesa lo recogemos con .then
-fetch ('${apiUrl}/users')
+fetch (`${apiUrl}/users`)
     .then(response => response.json()) // lo convierte en un json
     // 2-inicializo template con los datos que necesito (map del objeto inicial), 3-insertar dentro del elemento html la variable con los datos
     .then(users => {
         const template = users.map ((user) => `<li>${user.name} ${user.email}</li>`);
         HTMLelem.innerHTML = `<ul>${template}</ul>`;
-});
+    });
+*/
+
+// llamar a una API con método FETCH, usando ELEMENTOS DE HTML para formatear el html
+
+const apiUrl = "https://jsonplaceholder.typicode.com";
+
+const HTMLelem = document.querySelector ("#app");
+// creamos un document.fragment en el DOM del html
+// const template = createDocumentFragment(); template va a ser directamente el elemento ul
+const template = document.createElement('ul');
+
+fetch (`${apiUrl}/users`)
+    .then(response => response.json())
+    .then(users => {
+    // para cada user, creamos un elemento de tipo li. A este elemento le pasamos un hijo de tipo texto, que es el user y el mail.
+        users.forEach((user) => {
+            let elem = document.createElement('li');
+            elem.appendChild(document.createTextNode(`${user.name} ${user.email}`));
+            // y le añadimos el elem a template
+            template.appendChild(elem);
+        });
+    });
+// y finalmente le añadimos template al elemento inicial html
+HTMLelem.appendChild(template);
+
 
 
 
